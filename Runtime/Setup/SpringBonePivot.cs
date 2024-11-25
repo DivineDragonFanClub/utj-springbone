4188593f51bf977a926af2b6a69c0704ba3eda66
Alexander Ren <alex.ren@gmail.com>
2024-11-24 23:49:45 -0800

    add pivots

--- Runtime/Setup/SpringBonePivot.cs
+++ Runtime/Setup/SpringBonePivot.cs
@@ -1,8 +1,33 @@
 ﻿using UnityEngine;
+using UTJ.Jobs;
 
 namespace UTJ
 {
+    [ExecuteInEditMode]
     public class SpringBonePivot : MonoBehaviour
     {
+#if UNITY_EDITOR
+        void Update()
+        {
+            if (transform.hasChanged)
+            {
+                transform.hasChanged = false;
+                var myManager = GetComponentInParent<SpringJobManager>();
+                if (myManager != null)
+                {
+                    SpringJobManager.UpdateBoneList(myManager);
+                }
+            }
+        }
+
+        public void OnValidate()
+        {
+            var myManager = GetComponentInParent<SpringJobManager>();
+            if (myManager != null)
+            {
+                SpringJobManager.UpdateBoneList(myManager);
+            }
+        }
     }
+#endif
 }
